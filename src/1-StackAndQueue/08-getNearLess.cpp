9103d4be48b64f361893dee4ffa9d10ce1f9b5a0
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-02-09 22:37:39 +0800

    Update 08-getNearLess.cpp

--- src/1-StackAndQueue/08-getNearLess.cpp
+++ src/1-StackAndQueue/08-getNearLess.cpp
@@ -1,14 +1,14 @@
 #include"headFile.h"
 
-// µ¥µ÷Õ»½á¹¹
-// ÕÒÄ³Ò»¸öÎ»ÖÃ×ó±ß/ÓÒ±ßµÚÒ»¸ö±ÈËüĞ¡µÄÎ»ÖÃ
+// å•è°ƒæ ˆç»“æ„
+// æ‰¾æŸä¸€ä¸ªä½ç½®å·¦è¾¹/å³è¾¹ç¬¬ä¸€ä¸ªæ¯”å®ƒå°çš„ä½ç½®
 
-// ÎŞÖØ¸´Öµ
+// æ— é‡å¤å€¼
 class GetNearLessNoRepeat {
 public:
     vector<vector<int>> getNearLessNoRepeat(vector<int>& nums) {
         vector<vector<int>>ans(nums.size(), vector<int>(2));
-        stack<int>stack; // ´ÓÕ»¶¥µ½Õ»µ×µİ¼õ
+        stack<int>stack; // ä»æ ˆé¡¶åˆ°æ ˆåº•é€’å‡
         for (int i = 0; i < nums.size(); i++) {
             while (!stack.empty() && nums[stack.top()] > nums[i]) {
                 int popIndex = stack.top();
@@ -93,5 +93,100 @@ public:
         }
         cout << "end" << endl;
     }
+};
 
+// å¯èƒ½æœ‰é‡å¤å€¼çš„å•è°ƒæ ˆ
+class GetNearLess {
+public:
+    vector<vector<int>> getNearLess(vector<int>& nums) {
+        vector<vector<int>>res(nums.size(), vector<int>(2));
+        stack<vector<int>>stack;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!stack.empty() && nums[stack.top()[0]] > nums[i]) {
+                vector<int> popIndexs = stack.top();
+                stack.pop();
+                int leftLessIndex = stack.empty() ? -1 : stack.top().back();
+                for (int popi : popIndexs) {
+                    res[popi][0] = leftLessIndex;
+                    res[popi][1] = i;
+                }
+            }
+            if (!stack.empty() && nums[stack.top()[0]] == nums[i]) {
+                stack.top().push_back(i);
+            } else {
+                vector<int>list;
+                list.push_back(i);
+                stack.push(list);
+            }
+        }
+        // æ¸…ç®—
+        while (!stack.empty()) {
+            vector<int>popIndexs = stack.top();
+            stack.pop();
+            int leftLessIndex = stack.empty() ? -1 : stack.top().back();
+            for (int popi : popIndexs) {
+                res[popi][0] = leftLessIndex;
+                res[popi][1] = -1;
+            }
+        }
+        return res;
+    }
+
+    // for test
+    vector<vector<int>> getNearLess2(vector<int>& nums) {
+        vector<vector<int>>ans(nums.size(), vector<int>(2));
+        for (int i = 0; i < nums.size(); i++) {
+            int leftLessIndex = -1;
+            int rightLessIndex = -1;
+
+            int cur = i - 1;
+            while (cur >= 0) {
+                if (nums[cur] < nums[i]) {
+                    leftLessIndex = cur;
+                    break;
+                }
+                cur--;
+            }
+
+            cur = i + 1;
+            while (cur < nums.size()) {
+                if (nums[cur] < nums[i]) {
+                    rightLessIndex = cur;
+                    break;
+                }
+                cur++;
+            }
+
+            ans[i][0] = leftLessIndex;
+            ans[i][1] = rightLessIndex;
+        }
+        return ans;
+    }
+
+
+    vector<int> randomArray(int size, int l, int r) {
+        srand(time(0));
+        vector<int>nums;
+        for (int i = 0; i < size; i++) {
+            int num = rand() % (r - l + 1) + l;
+            nums.push_back(num);
+        }
+        return nums;
+    }
+
+    void test() {
+        srand(time(0));
+        for (int test = 0; test < 1000; test++) {
+            int size = rand() % (1000 - 1 + 1) + 1;
+            int k = rand() % (size - 1 + 1) + 1;
+            vector<int>nums = randomArray(size, 0, 1000);
+            vector<vector<int>>ans1 = getNearLess2(nums);
+            vector<vector<int>>ans2 = getNearLess(nums);
+            if (ans1 != ans2) {
+                cout << "fuck!!!" << endl;
+                break;
+            }
+        }
+        cout << "end" << endl;
+    }
 };
