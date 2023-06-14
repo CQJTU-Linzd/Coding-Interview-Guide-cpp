d1a9f569ce57954127b22b676ff4f8503fe4ff82
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-14 11:32:47 +0800

    Update headFile.h

--- src/headFile.h
+++ src/headFile.h
@@ -1,3 +1,6 @@
+#ifndef _HEADFILE_
+#define _HEADFILE_
+
 #pragma once
 #include<iostream>
 using namespace std;
@@ -13,3 +16,43 @@ using namespace std;
 #include<queue>
 #include<unordered_map>
 #include<unordered_set>
+#include<ctime>
+#include<cmath>
+
+class ListNode {
+public:
+	int val;
+	ListNode* next;
+	ListNode(int val) {
+		this->val = val;
+		this->next = nullptr;
+	}
+};
+
+// 双向链表
+class DoubleNode {
+public:
+	int val;
+	DoubleNode* next;
+	DoubleNode* last;
+	DoubleNode(int val) {
+		this->val = val;
+		this->next = nullptr;
+		this->last = nullptr;
+	}
+};
+
+// 二叉树
+class TreeNode {
+public:
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int val) {
+		this->val = val;
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+#endif // !_HEADFILE_
